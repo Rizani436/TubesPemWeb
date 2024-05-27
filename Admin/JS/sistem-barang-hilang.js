bb1c8f6e4da8043069719240c12a674c54450225
F1D022091_Rizal Dwi Kurniawan <115381579+RizalDwiKurniawan21@users.noreply.github.com>
2024-05-27 12:03:08 +0800

    Add files via upload

---
+++ Admin/JS/sistem-barang-hilang.js
@@ -0,0 +1,111 @@
+var header_left = document.querySelector('.menu-left');
+var header_left_menu = document.querySelector('.header-left-menu');
+var content = document.querySelector('.content');
+var tabel = document.querySelector('.content-top table');
+var judul_header = document.querySelector('.judul-header p');
+
+header_left.addEventListener('click', function(){
+    if (header_left_menu.style.display === 'none'){
+        header_left_menu.style.display = 'block';
+        // content.style.marginLeft = '20px';
+        tabel.style.width = '1175px';
+        content.style.marginLeft = '350px';
+        judul_header.style.marginLeft = '230px';
+    } else {
+        header_left_menu.style.display ='none';
+        tabel.style.width = '1700px';
+        judul_header.style.marginLeft = '0px';
+
+    }
+});
+
+var mengelola_sistem = document.querySelector('.header-left-menu .menu-bottom > ul > .menu-dropdown:nth-child(4) > button');
+var memverifikasi_informasi = document.querySelector('.header-left-menu .menu-bottom > ul > .menu-dropdown:nth-child(3) > button');
+var menu_dropdowns = document.querySelectorAll('.menu-dropdown');
+
+menu_dropdowns.forEach(function(menu_dropdown) {
+    var dropdown = menu_dropdown.querySelector('.dropdown');
+    menu_dropdown.addEventListener("click", function(){
+        if (dropdown.style.display == 'none'){
+            dropdown.style.display = 'block';
+            if (menu_dropdown.contains(memverifikasi_informasi)) {
+                mengelola_sistem.style.marginTop = '103px';
+            }
+        } else {
+            dropdown.style.display = 'none';
+            if (menu_dropdown.contains(memverifikasi_informasi)) {
+                mengelola_sistem.style.marginTop = '0px';
+            }
+        }
+    });
+});
+
+var close_menu = document.querySelector('.close-menu');
+var header_left_menu = document.querySelector('.header-left-menu');
+close_menu.addEventListener('click', function(){
+    if (header_left_menu.style.display === 'none'){
+        header_left_menu.style.display = 'block';
+    } else {
+        header_left_menu.style.display ='none';
+        content.style.marginLeft = '20px';
+        tabel.style.width = '1500px';
+        judul_header.style.marginLeft = '0px';
+    }
+});
+
+var akun_profil = document.querySelector('.akun-profil');
+var profil = document.querySelector('.header-right img');
+
+profil.addEventListener('click', function(){
+    if(akun_profil.style.display == 'none'){
+        akun_profil.style.display = 'block';
+    } else {
+        akun_profil.style.display = 'none';
+    }
+})
+
+var popUp_terima = document.querySelector('.popUp-terima');
+var popUp_tolak = document.querySelector('.popUp-tolak');
+var terima = document.querySelector('.footer-terima button[type="submit"]');
+var tutup = document.querySelector('.footer-terima button[type="button"');
+var btn_terima = document.querySelectorAll('.btn-terima');
+btn_terima.forEach(function(btn_terima) {
+    btn_terima.addEventListener('click', function(){
+        popUp_terima.style.display = 'flex';
+        document.body.classList.add('no-scroll');
+    });
+});
+
+tutup.addEventListener('click', function(){
+    popUp_terima.style.display = 'none';
+    document.body.classList.remove('no-scroll');
+});
+
+var tolak = document.querySelector('.footer-tolak button[type="submit"]');
+var tutup2 = document.querySelector('.footer-tolak button[type="button"');
+var btn_tolak = document.querySelectorAll('.btn-tolak');
+btn_tolak.forEach(function(btn_tolak) {
+    btn_tolak.addEventListener('click', function(){
+        popUp_tolak.style.display = 'flex';
+        document.body.classList.add('no-scroll');
+    });
+});
+
+tutup2.addEventListener('click', function(){
+    popUp_tolak.style.display = 'none';
+    document.body.classList.remove('no-scroll');
+});
+
+const formTerima = document.querySelector('.formTerima');
+const informasiDetail = document.querySelector('.informasiDetail');
+const pInformasiDetail = document.querySelector('.p-informasiDetail');
+
+formTerima.addEventListener('submit', function(event) {
+    if (informasiDetail.value.length > 255) {
+        pInformasiDetail.textContent = 'Panjang Maksikal Karakter 255';
+        pInformasiDetail.style.color = 'red';
+        event.preventDefault();
+    } else {
+        pInformasiDetail.textContent = '';
+    }
+});
